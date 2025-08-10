443d0ddff47ab9f06e8668887d49127d737e370c
Abdelrahman <abdelrahman.yousef@hadaf-hq.com>
2025-08-10 11:26:12 +0300

    edit the api registration

--- database/migrations/2025_08_10_082447_add_phone_column_to_users_table.php
+++ database/migrations/2025_08_10_082447_add_phone_column_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'user'])->default('user')->after('email');
+            if (!Schema::hasColumn('users', 'phone')) {
+                $table->string('phone', 20)->nullable()->after('role');
+            }
         });
     }
 
@@ -22,7 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
+            if (Schema::hasColumn('users', 'phone')) {
+                $table->dropColumn('phone');
+            }
         });
     }
 };
