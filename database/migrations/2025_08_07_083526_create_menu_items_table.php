e4d445f34b51715d177c3de25257fd86978c1b62
abdelrahmanYouseff <abdelrahman.yousef@hadaf-hq.com>
2025-09-08 10:05:28 +0300

    delete categories

--- database/migrations/2025_08_07_083526_create_menu_items_table.php
+++ database/migrations/2025_08_07_083526_create_menu_items_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('menu_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
