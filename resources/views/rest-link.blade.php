524cfdb8d7f445dc3333a5d14ea47bbf8496ad77
abdelrahmanYouseff <abdelrahman.yousef@hadaf-hq.com>
2025-10-01 13:40:33 +0300

    edit

--- resources/views/rest-link.blade.php
+++ resources/views/rest-link.blade.php
@@ -132,17 +132,6 @@
                 <p class="text-gray-500 text-sm">
                     <span id="powered-by-text">Powered by</span> <span class="font-semibold" style="color: #cf4823;">AdvFood</span>
                 </p>
-                <div class="flex justify-center space-x-4 mt-4">
-                    <a href="#" class="text-gray-400 hover:text-purple-600 transition-colors">
-                        <i class="fab fa-instagram text-xl"></i>
-                    </a>
-                    <a href="#" class="text-gray-400 hover:text-purple-600 transition-colors">
-                        <i class="fab fa-facebook text-xl"></i>
-                    </a>
-                    <a href="#" class="text-gray-400 hover:text-purple-600 transition-colors">
-                        <i class="fab fa-twitter text-xl"></i>
-                    </a>
-                </div>
             </div>
         </div>
     </div>
